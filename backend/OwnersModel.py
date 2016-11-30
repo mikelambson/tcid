ab214f4fd012384e633dc0d891f6d8368ffd4a62
mikelambson <mikelambson@users.noreply.github.com>
2016-11-30 07:15:09 -0800

    Update OwnersModel.py

--- backend/OwnersModel.py
+++ backend/OwnersModel.py
@@ -5,7 +5,7 @@ from components.validation import validate_word;
 
 class Owners(DB.Model):
 	id = DB.Column(DB.Integer, primary_key=True, autoincrement=True);
-	name = DB.Column(DB.Text);
+	name = DB.Column(DB.Varchar(80));
 	phone_id = DB.Column(DB.Integer, DB.ForeignKey('phones.id'));
 	notation = DB.Column(DB.Text, nullable=True);
 	created_by = DB.Column(DB.Integer, DB.ForeignKey('users.id'));
@@ -13,10 +13,6 @@ class Owners(DB.Model):
 	updated_by = DB.Column(DB.Integer, DB.ForeignKey('users.id'), nullable=True);
 	updated_at = DB.Column(DB.DateTime, nullable=True);
 
-"""	@validates('subject')
-	def validate_subject(self, key, subject):
-		return validate_word(subject, 2, 320, valid=re.compile('[a-zA-Z\s]+'))
-"""
 	def __init__(self, name, mailing_address, city, state, zip, notation, created_at, updated_at):
 		self.name = name;
 		self.mailing_address = mailing_address;
@@ -27,58 +23,3 @@ class Owners(DB.Model):
 		self.created_at = datetime.datetime.now();
 		self.updated_at = self.created_at;
 
-"""	def serialize(self, session):
-		if session.clearance:
-			return {"id": self.id, "subject": self.subject, "deleted_by": self.deleted_by, "created_by": self.created_by, "created_at": self.created_at.strftime("%B %d, %Y %H:%M:%S"), "updated_at": self.updated_at.strftime("%B %d, %Y %H:%M:%S"), "updated_by": self.updated_by};
-		else: #public or protected
-			return {"delete": self.id} if self.deleted_by else {"id": self.id, "subject": self.subject};
-
-	@staticmethod
-	def create(args, secure):
-		auth = DB.Models["sessions"].authenticate(secure, paranoid=True, role="admin");
-		if auth.success == False:
-			return auth.serialize();
-		else:
-			security_question = SecurityQuestions(args["subject"], auth.session.user);
-			DB.session.add(security_question);
-			DB.session.commit();
-			return {"success": True};
-
-	@staticmethod
-	def exists(prop, value): 
-		if prop == "subject": 
-			return True if SecurityQuestions.query.filter_by(subject=value).count() else False;
-		else: 
-			return False;#invalid property
-
-	@staticmethod
-	def request(mode, secure):
-		auth = DB.Models["sessions"].authenticate(secure, bypass_activation=True);
-		if auth.success == False:
-			return auth.serialize();
-		cache = DB.Models["caches"].cache("security_questions", mode, auth.session, False);
-		if mode == "config": 
-			data = [i.serialize(auth.session) for i in SecurityQuestions.query.filter(SecurityQuestions.updated_at > cache).all()] if cache else [i.serialize(auth.session) for i in SecurityQuestions.query.all()];
-		elif mode == "index": #public 
-			data = [i.serialize(auth.session) for i in SecurityQuestions.query.filter(SecurityQuestions.updated_at > cache, SecurityQuestions.deleted_by == None).all()] if cache else [i.serialize(auth.session) for i in SecurityQuestions.query.all()];
-		else: #login
-			data = [i.serialize(auth.session) for i in SecurityQuestions.query.filter(SecurityQuestions.updated_at > cache, SecurityQuestions.deleted_by == None, SecurityQuestions.id == auth.session.user.security_question.id).all()] if cache else [i.serialize(auth.session) for i in SecurityQuestions.query.filter_by(id = auth.session.user.security_question.id).all()];
-		return {"success": True, "items": len(data), "data": data};
-
-	@staticmethod
-	def update(args, secure):
-		auth = DB.Models["sessions"].authenticate(secure, paranoid=True, role="admin");
-		if auth.success == False:
-			return auth.serialize();
-		else:
-			security_question = SecurityQuestions.query.get(args["id"]);
-			for param, value in args.iteritems():
-				if param == "subject":
-					security_question.subject = value;
-				elif param == "deleted_by":
-					security_question.deleted_by = auth.session.user if value == True else None;
-			security_question.updater = auth.session.user;
-			security_question.updated_at = datetime.datetime.now();
-			DB.session.commit();
-			return {"success": True};
-"""			
