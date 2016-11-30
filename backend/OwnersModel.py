60349f50708b2a7ac24bac017379290849f49224
mikelambson <mikelambson@users.noreply.github.com>
2016-11-30 09:06:58 -0800

    Update OwnersModel.py

--- backend/OwnersModel.py
+++ backend/OwnersModel.py
@@ -6,19 +6,14 @@ from components.validation import validate_word;
 class Owners(DB.Model):
 	id = DB.Column(DB.Integer, primary_key=True, autoincrement=True);
 	name = DB.Column(DB.Varchar(80));
-	phone_id = DB.Column(DB.Integer, DB.ForeignKey('phones.id'));
 	notation = DB.Column(DB.Text, nullable=True);
 	created_by = DB.Column(DB.Integer, DB.ForeignKey('users.id'));
 	created_at = DB.Column(DB.DateTime);
 	updated_by = DB.Column(DB.Integer, DB.ForeignKey('users.id'), nullable=True);
 	updated_at = DB.Column(DB.DateTime, nullable=True);
 
-	def __init__(self, name, mailing_address, city, state, zip, notation, created_at, updated_at):
+	def __init__(self, name, notation, created_at, updated_at):
 		self.name = name;
-		self.mailing_address = mailing_address;
-		self.city = city;
-		self.state = state;
-		self.zip = zip;
 		self.notation = notation;
 		self.created_at = datetime.datetime.now();
 		self.updated_at = self.created_at;
