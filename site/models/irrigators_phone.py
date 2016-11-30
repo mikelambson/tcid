88c2eed39c9828dbe7ddc4ed6e03637a8ff0cc62
mikelambson <mikelambson@users.noreply.github.com>
2016-11-30 13:08:39 -0800

    Update irrigators_phone.py

--- site/models/irrigators_phone.py
+++ site/models/irrigators_phone.py
@@ -10,7 +10,7 @@ class Irrigators_phone(DB.Model):
 	primary_phone = DB.Column(DB.Boolean);
 	created_by = DB.Column(DB.Integer, DB.ForeignKey('users.id'));
 	created_at = DB.Column(DB.DateTime);
-	updated_by = DB.Column(DB.Integer, DB.ForeignKey('users.id'), nullable=True);
+	updated_by = DB.Column(DB.Integer, DB.ForeignKey('users.id'));
 	updated_at = DB.Column(DB.DateTime, nullable=True);
 
 	def __init__(self, primary_phone, created_at, updated_at):
