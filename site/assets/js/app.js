2540f7212fbf638f7c6086aa031f9de5be5e85b6
mikelambson <macmicool@gmail.com>
2016-12-01 07:41:06 -0800

    include

---
+++ site/assets/js/app.js
@@ -0,0 +1,19 @@
+'use strict';   // See note about 'use strict'; below
+
+var myApp = angular.module('myApp', [
+ 'ngRoute',
+]);
+
+myApp.config(['$routeProvider',
+     function($routeProvider) {
+         $routeProvider.
+             when('/', {
+                 templateUrl: '/templates/partials/index.html',
+             }).
+             when('/about', {
+                 templateUrl: '../templates/partials/about.html',
+             }).
+             otherwise({
+                 redirectTo: '/'
+             });
+    }]);
