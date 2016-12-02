5f6870445e7a1e9f8a8dc6a19df73da7bc485b49
mikelambson <mikelambson@users.noreply.github.com>
2016-12-02 09:10:27 -0800

    Update app.component.ts

--- site/app/app.component.ts
+++ site/app/app.component.ts
@@ -2,7 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { Component } from '@angular/core';
-@Component({
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+
   selector: 'my-app',
   template: `
     <div class="jumbotron">
