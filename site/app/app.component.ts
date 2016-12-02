59626b3bc2e103f234464b1982293db618b87bba
mikelambson <mikelambson@users.noreply.github.com>
2016-12-02 09:08:18 -0800

    Update app.component.ts

--- site/app/app.component.ts
+++ site/app/app.component.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
