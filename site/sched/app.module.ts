062047e1feef3f4cbdb4ceb8e39e7aba00805e18
mikelambson <mikelambson@users.noreply.github.com>
2016-12-02 16:12:28 -0800

    Update app.module.ts

--- site/sched/app.module.ts
+++ site/sched/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }        from './app.component';
 import { HeaderComponent}   from './header.component';
 import { MainComponent } from './main.component';
 import { FooterComponent }     from './footer.component';
