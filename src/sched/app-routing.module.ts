f6fbac25628c3b62a01a335900b2729b7e13ef0a
mikelambson <mikelambson@users.noreply.github.com>
2016-12-06 07:01:37 -0800

    Create app-routing.module.ts

---
+++ src/sched/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }              from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+import { HomeComponent }         from './home.component';
+import { LoginComponent }        from './login.component';
+import { ScheduleComponent }     from './schedule.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home',  component: HomeComponent },
+  { path: 'detail/:id', component: LoginComponent },
+  { path: 'schedule',     component: ScheduleComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
