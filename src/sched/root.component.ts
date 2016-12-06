3b289c06c08f77b915087d9473a997eaa831b668
mikelambson <mikelambson@users.noreply.github.com>
2016-12-06 06:33:53 -0800

    Update root.component.ts

--- src/sched/root.component.ts
+++ src/sched/root.component.ts
@@ -1,8 +1,11 @@
 // root.component.ts
 import {Component} from 'angular2/core';
+import {RouteConfig} from 'angular2/router';
+
 import {Home} from './home/home.component.ts';
 import {Login} from './login/login.component.ts';
 import {Schedule} from './shedule/schedule.component.ts';
+
 
 @Component({
   selector: 'my-app',
@@ -16,6 +19,13 @@ import {Schedule} from './shedule/schedule.component.ts';
     </div>
   `
 })
+
+@RouteConfig([
+  { path: '/', name: 'Home', component: Home, useAsDefault: true },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/schedule', name: 'Schedule', component: Schedule },
+])
+
 export class AppComponent {
 
 }
