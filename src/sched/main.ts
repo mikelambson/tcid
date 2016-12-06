f7b81bc4f46407266193e78c9f319cca76de531c
mikelambson <mikelambson@users.noreply.github.com>
2016-12-06 06:27:51 -0800

    Update main.ts

--- src/sched/main.ts
+++ src/sched/main.ts
@@ -1,5 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-import {ROUTER_PROVIDERS} from 'angular2/router';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
