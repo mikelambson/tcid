30f83f8f176efe427b9336c497c9de544e4d2de7
mikelambson <mikelambson@users.noreply.github.com>
2016-12-06 06:14:30 -0800

    Update root.component.ts

--- src/sched/root.component.ts
+++ src/sched/root.component.ts
@@ -1,11 +1,15 @@
 // root.component.ts
 import {Component} from 'angular2/core';
+import {Home} from './home/home.component.ts';
+import {Login} from './login/login.component.ts';
+import {Schedule} from './shedule/schedule.component.ts';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="app">
-      Hello world!
+      Hello world! 
+      <p>Root Component Here</p>
       <main>
         <!-- stuff -->
       </main>
