99b2a6efa861c10a9a71e7e2e9584e66d66dfe5d
mikelambson <mikelambson@users.noreply.github.com>
2016-12-06 06:35:52 -0800

    Create schedule.component.ts

---
+++ src/sched/schedule.component.ts
@@ -0,0 +1,14 @@
+/schedule/schedule.component.ts
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'schedule',
+  template: `
+    <h1>Schedule view!</h1>
+  `
+})
+export class Schedule {
+  constructor() {
+
+  }
+}
