5169e7dba56eb29c73acf5f7dfba9a381d3ba92d
mikelambson <mikelambson@users.noreply.github.com>
2016-12-06 06:23:25 -0800

    Create login.component.ts

---
+++ src/sched/login.component.ts
@@ -0,0 +1,14 @@
+/login/login.component.ts
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'login',
+  template: `
+    <h1>Login view!</h1>
+  `
+})
+export class Login {
+  constructor() {
+
+  }
+}
