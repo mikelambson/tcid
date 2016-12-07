f98085094271f4f39a204989e078eeaf914f196c
mikelambson <mikelambson@users.noreply.github.com>
2016-12-06 19:14:12 -0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {ROUTER_PROVIDERS} from 'angular/router';
 import { FormsModule }    from '@angular/forms';
 
 import { AppComponent }          from './app.component';
