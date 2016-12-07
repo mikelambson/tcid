8a29fa97a3b2e880c0771eea57f5f02707d13a89
mikelambson <mikelambson@users.noreply.github.com>
2016-12-06 19:33:54 -0800

    Update heroes.component.ts

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -7,8 +7,8 @@ import { HeroService } from './hero.service';
 @Component({
   moduleId: module.id,
   selector: 'my-heroes',
-  templateUrl: 'heroes.component.html',
-  styleUrls: [ 'heroes.component.css' ]
+  templateUrl: 'templates/partials/heroes.component.html',
+  styleUrls: [ 'asstets/css/heroes.component.css' ]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
