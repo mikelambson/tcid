3ac86f405d64146d387091d686204010bc0117b6
mikelambson <mikelambson@users.noreply.github.com>
2016-12-07 12:06:02 -0800

    Update boot.ts

--- src/assets/boot.ts
+++ src/assets/boot.ts
@@ -1,4 +1,4 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppComponent);
