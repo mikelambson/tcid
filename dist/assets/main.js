939a404a01ac742407259421f6d7d60b0ea6ee0b
mikelambson <macmicool@gmail.com>
2016-12-08 05:51:45 -0800

    dev 1208160547

--- dist/assets/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var master_module_1 = require("./master.module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(master_module_1.MasterModule);
