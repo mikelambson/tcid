a4e75e66e93b2a7e0887c48c43ca6d3837b849bf
mikelambson <mikelambson@users.noreply.github.com>
2016-12-09 07:51:48 -0800

    Update master.module.ts

--- src/assets/master.module.ts
+++ src/assets/master.module.ts
@@ -3,11 +3,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MasterComponent }  from './master.component';
-//import { HeaderComponent }	from './header/header.component';
+import { HeaderComponent }	from './header/header.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [MasterComponent, /*HeaderComponent*/],
+  declarations: [MasterComponent, HeaderComponent],
   bootstrap: [ MasterComponent ],
 })
 export class MasterModule { }
