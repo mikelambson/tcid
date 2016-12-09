b9a25aca606dd265d9ff2e973db8bafae6be260c
mikelambson <mikelambson@users.noreply.github.com>
2016-12-09 10:06:17 -0800

    Update header.component.ts

--- src/assets/header/header.component.ts
+++ src/assets/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'header',
-	templateUrl: '/assets/parts/header-component.html',
+	templateUrl: './assets/parts/header-component.html',
+	styleUrls: [ './assets/css/header.component.css' ],
 })
 export class HeaderComponent {}
