01cd05a62015803ee903342e990a63b51829cd3d
mikelambson <mikelambson@users.noreply.github.com>
2016-12-09 08:58:52 -0800

    Update master.component.ts

--- src/assets/master.component.ts
+++ src/assets/master.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  templateUrl: './master-component.html',
+  templateUrl: '/assets/master-component.html',
   styleUrls: [],
   })
 export class MasterComponent  { name = 'Master'; }
