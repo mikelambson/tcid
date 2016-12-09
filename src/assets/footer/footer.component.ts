16c9a18d0aaa1255f1cfeaa98032e43d10371d93
mikelambson <mikelambson@users.noreply.github.com>
2016-12-09 15:28:16 -0800

    Update footer.component.ts

--- src/assets/footer/footer.component.ts
+++ src/assets/footer/footer.component.ts
@@ -1,1 +1,9 @@
 //footer.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'footer',
+	templateUrl: './assets/parts/footer-component.html',
+	styleUrls: [ './assets/css/footer.component.css ],
+})
+export class FooterComponent {}
