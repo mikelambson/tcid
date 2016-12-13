e4275aed033fc9e38e01c5747205f0c912e32288
mikelambson <mikelambson@users.noreply.github.com>
2016-12-13 09:37:25 -0800

    Update 404.ts

--- src/assets/components/error/404.ts
+++ src/assets/components/error/404.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-	templateUrl: "/components/404.html"
+	templateUrl: "/components/404.html",
+	styleUrls: [ './assets/css/404.css' ],
 })
 
 export class Error404Component { 
