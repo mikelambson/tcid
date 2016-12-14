a577ea66d2098e4be1788258de3289bc69b09684
mikelambson <macmicool@gmail.com>
2016-12-14 08:50:31 -0800

    navbar added

---
+++ src/assets/components/header/navigation.component.ts
@@ -0,0 +1,30 @@
+// navigation.component.ts
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+    selector: 'navigation',
+    template: `
+        <nheader>
+            <nav class='navbar navbar-sticky-top navbar-dark bg-inverse'>
+                <span class='navbar-brand'>I Wish My Nav Was Automated</span>
+                <ul class='nav navbar-nav pull-xs-right'>
+                    <li class='nav-item'>
+                        <a class='nav-link' [routerLink]="['/home']">Home</a>
+                    </li>
+                    <li class='nav-item'>
+                        <a class='nav-link' [routerLink]="['/schedule']">Schedule</a>
+                    </li>
+                    <li class='nav-item'>
+                        <a class='nav-link' [routerLink]="['/manage']">Manage</a>
+                    </li>
+                    <li class='nav-item'>
+                        <a class='nav-link' [routerLink]="['/profile']">Profile</a>
+                    </li>
+                </ul>
+            </nav>
+        </nheader>
+    `,
+    directives: [ROUTER_DIRECTIVES]
+})
+export class NavigationComponent {}
