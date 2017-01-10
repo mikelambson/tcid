919a78358f75461b0b923192b287b6b1f365eb37
mikelambson <macmicool@gmail.com>
2017-01-10 14:13:53 -0800

    order function

--- src/assets/components/schedule/order.component.ts
+++ src/assets/components/schedule/order.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 import { Order } from './order';
 import { OrderService } from './order.service';
@@ -19,10 +20,12 @@ export class OrderComponent implements OnInit {
   constructor(private orderService: OrderService, private http: Http) { }
   getOrders(): void {
     this.http.get('/schedule')
-      .map((response: Response): Order[] => response.json())
-      .subscribe((orders: Response): void => this.orders = orders, 
-        (error: string): void => console.log('An unexpected error has occurred: '+error), 
-        (): void => console.log('Get request completed. Remind me to teach you about Observables Mike.'));
+    .map((response: Response): {}[] => response.json())
+    .flatMap((data: {}[]): Observable<{}> => Observable.from(data))
+    .map((data: {}): Order => new Order(data))
+    .subscribe((order: Order): number => this.orders.push(order),  
+      (error: string): void => console.log('An unexpected error has occurred: '+error), 
+      (): void => console.log('Get request completed. Remind me to teach you about Observables Mike.'));
     //what's above will replace what's below
     this.orderService.getOrders().then(orders => this.orders = orders);
   }
--- src/assets/components/schedule/order.ts
+++ src/assets/components/schedule/order.ts
@@ -12,4 +12,19 @@ export class Order {
 	approx_hrs: string;
 	approx_af: string;
 	calc: string;
+
+	constructor(data: {} = {}) {
+	this.order_no = data["order_no"] || "";
+	this.scheduled = data["scheduled"] || "";
+	this.lateral = data["lateral"] || "";
+	this.start_time = data["start_time"] || "";
+	this.checks = data["checks"] || "";
+	this.stop_time = data["stop_time"] || "";
+	this.status = data["status"] || "";
+	this.approx_cfs = data["approx_cfs"] || "";
+	this.approx_hrs = data["approx_hrs"] || "";
+	this.approx_af = data["approx_af"] || "";
+	this.calc = let result: number = stop_time == null ? (DATEDIFF(minute, start_time, Date()) / 60.0) * cfs * 0.0825 : (DATEDIFF(minute, start_time, stop_time) / 60.0) * cfs * 0.0825;
+	};
+	
 }
