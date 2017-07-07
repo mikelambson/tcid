c2d67caf987427bf0161d1a75160af79b76c8a97
mikelambson <mikelambson@users.noreply.github.com>
2017-07-07 13:07:16 -0700

    Update order.ts

--- src/assets/components/schedule/order.ts
+++ src/assets/components/schedule/order.ts
@@ -25,12 +25,12 @@ export class Order {
 	this.approx_cfs = data["approx_cfs"] || "";
 	this.approx_hrs = data["approx_hrs"] || "";
 	this.approx_af = data["approx_af"] || "";
-	this.calc = {{afcalc}};
+	this.calc = {{this.stop_time == null ? ((Date.parse(Date.now()) - Date.parse(this.start_time)) / 1000.0 / 60.0 / 60.0) * this.checks * 0.0825 : ((Date.parse(this.stop_time) - Date.parse(this.start_time)) /1000.0 / 60.0 / 60.0) * this.checks * 0.0825}};
 	};
 	
-	ngOnInit(){ 
-		new afcalc = this.stop_time == null ? ((Date.parse(Date.now()) - Date.parse(this.start_time)) / 1000.0 / 60.0 / 60.0) * this.checks * 0.0825 : ((Date.parse(this.stop_time) - Date.parse(this.start_time)) /1000.0 / 60.0 / 60.0) * this.checks * 0.0825
-	};
+	//ngOnInit(){ 
+	//	new afcalc = this.stop_time == null ? ((Date.parse(Date.now()) - Date.parse(this.start_time)) / 1000.0 / 60.0 / 60.0) * this.checks * 0.0825 : ((Date.parse(this.stop_time) - Date.parse(this.start_time)) /1000.0 / 60.0 / 60.0) * this.checks * 0.0825
+	//};
 };
 
 
