bf915e7d1122b3cfcc13b33b8d6cc11d04f0ab32
mikelambson <mikelambson@users.noreply.github.com>
2017-07-07 11:36:48 -0700

    Update order.ts

--- src/assets/components/schedule/order.ts
+++ src/assets/components/schedule/order.ts
@@ -26,11 +26,10 @@ export class Order {
 	this.approx_cfs = data["approx_cfs"] || "";
 	this.approx_hrs = data["approx_hrs"] || "";
 	this.approx_af = data["approx_af"] || "";
-	this.calc = afcalc["CALC"]
+	this.calc = data["afcalc"]
 	};
 	
-	function afcalc() {
-	return CALC(this.stop_time == null ? ((Date.parse(Date()) - Date.parse(this.start_time)) / 1000.0 / 60.0 / 60.0) * this.checks * 0.0825 : ((Date.parse(this.stop_time) - Date.parse(this.start_time)) /1000.0 / 60.0 / 60.0) * this.checks * 0.0825);
+	afcalc() => { this.stop_time == null ? ((Date.parse(Date()) - Date.parse(this.start_time)) / 1000.0 / 60.0 / 60.0) * this.checks * 0.0825 : ((Date.parse(this.stop_time) - Date.parse(this.start_time)) /1000.0 / 60.0 / 60.0) * this.checks * 0.0825;
 	};	
 };
 
