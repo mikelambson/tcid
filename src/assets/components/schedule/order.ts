a17ad9a524f8bfa672082f26dc95ef1d81ab5582
mikelambson <mikelambson@users.noreply.github.com>
2017-07-07 13:26:06 -0700

    Update order.ts

--- src/assets/components/schedule/order.ts
+++ src/assets/components/schedule/order.ts
@@ -29,8 +29,8 @@ export class Order {
 	};
 	
 	ngOnInit(){ 
-		new afcalc = this.stop_time == null ? ((Date.parse(Date.now()) - Date.parse(this.start_time)) / 1000.0 / 60.0 / 60.0) * this.checks * 0.0825 : ((Date.parse(this.stop_time) - Date.parse(this.start_time)) /1000.0 / 60.0 / 60.0) * this.checks * 0.0825;
-		this.calc = "afcalc";
+		var afcalc = this.stop_time == null ? ((Date.parse(Date.now()) - Date.parse(this.start_time)) / 1000.0 / 60.0 / 60.0) * this.checks * 0.0825 : ((Date.parse(this.stop_time) - Date.parse(this.start_time)) /1000.0 / 60.0 / 60.0) * this.checks * 0.0825;
+		this.calc = afcalc();
 	
 	};
 };
