78aa0deb5322b6f821889990d4c2e6786b037cef
mikelambson <mikelambson@users.noreply.github.com>
2017-07-13 15:23:19 -0700

    Update routes.ts

--- src/assets/components/master/routes.ts
+++ src/assets/components/master/routes.ts
@@ -8,6 +8,8 @@ export const routingProviders: any[] = [
 const MasterRoutes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'schedule', redirectTo: '/schedule', pathMatch: 'full' },
+    { path: 'manage', redirectTo: '/manage', pathMatch: 'full' },
+    { path: 'admin', redirectTo: '/admin', pathMatch: 'full' },
     { path: '**', component: Error404Component }
 ];
 
