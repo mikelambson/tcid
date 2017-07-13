adbfeef67f8cd56366bd40c0428effff182151af
mikelambson <mikelambson@users.noreply.github.com>
2017-07-13 15:13:20 -0700

    Update admin.module.ts

--- src/assets/components/admin/admin.module.ts
+++ src/assets/components/admin/admin.module.ts
@@ -1,1 +1,15 @@
 //admin.module.ts
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AdminIndexComponent} from './admin.component';
+import {ROUTING, routingProviders} from './routes';
+
+
+@NgModule({
+	imports: [CommonModule, ROUTING], 
+	declarations: [AdminIndexComponent], 
+	providers: [routingProviders]
+})
+export class AdminModule {
+	private name: string = 'Master'; 
+}
