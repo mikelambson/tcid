c7a82e4c6a06214abadcaf6617a2e4cf09ead9b3
mikelambson <mikelambson@users.noreply.github.com>
2017-07-13 15:11:10 -0700

    Create routes.ts

---
+++ src/assets/components/admin/routes.ts
@@ -0,0 +1,10 @@
+import { ModuleWithProviders } from "@angular/core";
+import {RouterModule} from '@angular/router';
+import {AdminIndexComponent} from './home.component';
+
+export const routingProviders: any[] = [
+];
+
+export const ROUTING: ModuleWithProviders = RouterModule.forChild([
+    { path: 'admin', component: AdminIndexComponent},
+]);
