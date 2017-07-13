3d9d2cc59a0373270199eb3974e1d5d04fdbefaf
mikelambson <mikelambson@users.noreply.github.com>
2017-07-13 14:30:39 -0700

    Create schedulemenu.component.ts

---
+++ src/assets/components/schedule/schedulemenu.component.ts
@@ -0,0 +1,13 @@
+//schedulemenu.component.ts
+
+/**This is a placeholder for the scheduling submenu.
+Must have as main selectors, the divisions: Truckee, West, Central, East
+Next will be the main heads in each selector:
+
+Truckee contains: TC-Head, Hazen-Head, Bench-Head, RD-Head
+West contains: T-Head, N-Head, VAE-Head
+Central contains: A-Head, G-Head, L-Head, L1-Head, NDOW-Head
+East contains: VDS-Head, S-Head, RE-Head, ST-Head, USFW-Head, Canvas-Head, Dutch-Head
+
+Under each main head will be up to 6 numbered heads. 
+Choosing a numbered head will display all orders assigned to corresponding head.
