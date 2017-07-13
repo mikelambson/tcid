2d98e586c78b6dc630ae5f019c04da99f6747e51
mikelambson <mikelambson@users.noreply.github.com>
2017-07-13 15:14:42 -0700

    Update admin.component.ts

--- src/assets/components/admin/admin.component.ts
+++ src/assets/components/admin/admin.component.ts
@@ -1,1 +1,13 @@
 //admin.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tcid',
+  templateUrl: './components/admin.html'
+  })
+
+export class AdminIndexComponent  { 
+	private name: string = "Master"
+  	constructor() {
+  	}
+}
