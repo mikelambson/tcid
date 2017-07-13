301367c59895c66a89bd768937285e918f7c4aa6
mikelambson <mikelambson@users.noreply.github.com>
2017-07-13 09:53:57 -0700

    Update order.service.ts

--- src/assets/components/schedule/order.service.ts
+++ src/assets/components/schedule/order.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 
 import { Order } from './order';
-import { ORDERS } from './mock-orders';
+import { ORDERS } from './mock-orders'; //this will eventually need to be the http connect for the database
 
 @Injectable()
 export class OrderService {
