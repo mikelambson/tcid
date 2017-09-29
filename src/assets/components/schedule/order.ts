efb56354ad9af7092419519bd21771fa3842dc45
Mike Lambson <macmicool@gmail.com>
2017-09-29 21:20:31 +0000

    work on af calc

--- src/assets/components/schedule/order.ts
+++ src/assets/components/schedule/order.ts
@@ -14,28 +14,29 @@ export class Order {
 	approx_cfs: string;
 	approx_hrs: string;
 	approx_af: string;
-	transform(calc: string) {
-		let a = new Date().amDifference(start_time);
-		let b = new stop_time().amDifference(start_time);
-		calc = !stop_time ? a * checks * 0.0825 : b * checks * 0.0825;
-		return calc;
-		};
+	calc: any;
+
 	
 	constructor(data: {} = {}) {
-	this.order_no = data["order_no"] || "";
-	this.scheduled = data["scheduled"] || "";
-	this.lateral = data["lateral"] || "";
-	this.start_time = data["start_time"] || "";
-	this.checks = data["checks"] || "";
-	this.stop_time = data["stop_time"] || "";
-	this.status = data["status"] || "";
-	this.approx_cfs = data["approx_cfs"] || "";
-	this.approx_hrs = data["approx_hrs"] || "";
-	this.approx_af = data["approx_af"] || "";
-	//this.calc = af();
-	//Calculate the order's acre footage from the stop time, or to current time if order is still running.
-	console.log(this.calc);
-	
+		this.order_no = data["order_no"] || "";
+		this.scheduled = data["scheduled"] || "";
+		this.lateral = data["lateral"] || "";
+		this.start_time = data["start_time"] || "";
+		this.checks = data["checks"] || "";
+		this.stop_time = data["stop_time"] || "";
+		this.status = data["status"] || "";
+		this.approx_cfs = data["approx_cfs"] || "";
+		this.approx_hrs = data["approx_hrs"] || "";
+		this.approx_af = data["approx_af"] || "";
+		//Calculate the order's acre footage from the stop time, or to current time if order is still running.
+		console.log(this.calc);
 	};
 
+	transform(calc: number) {
+	let a = new Date().amDifference(this.start_time);
+	let b = new this.stop_time().amDifference(this.start_time);
+	calc = !this.stop_time ? a * this.checks * 0.0825 : b * this.checks * 0.0825;
+	return calc;
+	};
+	
 };
